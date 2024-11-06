4e863c6403d910cd22a7d3601f981ddcd3dfa0e9
Jose Alberto Martin Torres <jalberto.martin@babelgroup.com>
2024-11-06 18:01:40 +0100

    Project init

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import "./App.css";
+/**
+ * TODO: usando la api de dragon ball https://web.dragonball-api.com/
+ * 1. Crear un componente que muestre un personaje de dragon ball
+ * 2. Crear un componente que muestre una lista de personajes de dragon ball
+ * 3. Crear un componente que muestre un personaje de dragon ball y que permita agregarlo a un listado de personajes favoritos
+ *
+ *
+ * R1: debe tener al menos un provider con toda la data de los personajes incluyendo el detalle de estos
+ * R2: debe tener al menos un consumer que permita acceder a la data de los personajes en una ruta separada
+ * EXTRA: montar un buscador de personajes
+ * EXTRA2: cachear la data de los personajes
+ */
+
+function App() {
+  return <></>;
+}
+
+export default App;
